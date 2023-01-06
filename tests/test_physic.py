ef767de517645c4bae542034aca8ece4ee729fc8
Andrey Smekhov <myfearismyblue@gmail.com>
2023-01-06 21:39:24 +0300

    Add tests for FigureState
    Same behaviour as FieldState

--- tests/test_physic.py
+++ tests/test_physic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tetris.physic import FieldState
+from tetris.physic import FieldState, FigureState
 
 
 @pytest.mark.parametrize('expected, args, kwargs',
@@ -25,3 +25,27 @@ def test_FieldState(expected,  args, kwargs):
 def test_FieldState_exceptions(expected_exception, args, kwargs):
     with pytest.raises(expected_exception):
         FieldState(*args, **kwargs)
+
+
+@pytest.mark.parametrize('expected, args, kwargs',
+                         [([[]], [[[]]], {}),
+                          ([[1, 2, 3], [1, 2, 3]], [[[1, 2, 3], [1, 2, 3]]], {}),  # args[0] == [[1, 2, 3], [1, 2, 3]]
+                          ([[0, 0], [0, 0]], [], {'width': 2, 'height': 2}),
+                          ([[0, 0], [0, 0]], [], {'state': [[0, 0], [0, 0]]}),
+                          ([[]], [], {'width': 0, 'height': 0}),
+                          ([[0, 0], [0, 0]], [None], {'width': 2, 'height': 2}),
+                          ])
+def test_FigureState(expected,  args, kwargs):
+    assert FigureState(*args, **kwargs) == expected
+
+
+@pytest.mark.parametrize('expected_exception, args, kwargs',
+                         [(TypeError, [], {}),
+                          (TypeError, [[]], {}),        # here args[0] = [] so state is flat []
+                          (TypeError, [], {'wrong': 1, 'height': 1}),
+                          (TypeError, [], {'width': 1, 'wrong': 1}),
+                          (TypeError, [[]], {'width': 1, 'height': 1})      # here args[0] = [] so state is flat []
+                          ])
+def test_FigureState_exceptions(expected_exception, args, kwargs):
+    with pytest.raises(expected_exception):
+        FigureState(*args, **kwargs)
